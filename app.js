8e4c93097330c39c6b66522d631d5558cb6bd852
Saroj <saroj_tamang@aol.com>
2024-01-18 16:51:44 +0000

    completed task 7

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const getHealthCheck = require("./controller/healthCheck.controller");
 const getTopicsData = require("./controller/getTopics.controller");
 const getApi = require("./controller/getapi.controller");
-const {getArticleByID, getArticle, getCommentByArtcId} = require("./controller/getArticleId.controller");
+const {getArticleByID, getArticle, getCommentByArtcId, postComment} = require("./controller/getArticleId.controller");
 
 app.use(express.json());
 
@@ -12,13 +12,19 @@ app.get("/api/topics", getTopicsData);
 app.get("/api", getApi);
 app.get("/api/articles/:article_id", getArticleByID);
 app.get('/api/articles', getArticle)
-
 app.get('/api/articles/:articles_id/comments', getCommentByArtcId)
 
+app.post('/api/articles/:articles_id/comments', postComment)
+
 app.use((err, req, res, next) => {
-  if (err.code === "22P02") {
+  if (err.code === "22P02"|| err.code === '23502') {
     res.status(400).send({ msg: "Bad request" });
   } else {
+    if (err.code === '23503') {
+
+      res.status(404).send({msg: 'Not found'})
+    }
+
     res.status(404).send({ msg: err.passThisMsg });
   }
 });
--- model/fetchAtricle.model.js
+++ model/fetchAtricle.model.js
@@ -39,4 +39,11 @@ const fetchComment = (id) => {
   })
 }
 
-module.exports = { fetchArticleData, fetchArticle,fetchComment };
+const insertComment = ({ username, body }, article_id) => {
+ 
+  return db.query(`INSERT INTO comments (author, body, article_id) VALUES ($1, $2, $3) RETURNING *`, [username, body, article_id]).then(({ rows }) => {
+    return rows[0]
+  })
+}
+
+module.exports = { fetchArticleData, fetchArticle,fetchComment,insertComment };
