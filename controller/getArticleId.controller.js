62a8658c42f2a3a7b614ed092a2c1a569a69ed94
Saroj <saroj_tamang@aol.com>
2024-01-19 13:55:34 +0000

    completed task 8

--- controller/getArticleId.controller.js
+++ controller/getArticleId.controller.js
@@ -1,7 +1,9 @@
 const {
   fetchArticleData,
   fetchArticle,
-  fetchComment, insertComment
+  fetchComment,
+  insertComment,
+  changeArticleVotes,
 } = require("../model/fetchAtricle.model");
 
 const getArticleByID = (req, res, next) => {
@@ -23,21 +25,38 @@ const getArticle = (req, res, next) => {
 };
 
 const getCommentByArtcId = (req, res, next) => {
-
-  const { articles_id } = req.params
-  fetchComment(articles_id).then(article => {
-    res.status(200).send({article})
-  }).catch(next)
-}
+  const { articles_id } = req.params;
+  fetchComment(articles_id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch(next);
+};
 
 const postComment = (req, res, next) => {
-  const { articles_id } = req.params
+  const { articles_id } = req.params;
   const newComment = req.body;
-  insertComment(newComment, articles_id).then((comment) => {
-    res.status(201).send({comment})
-  }).catch(next)
-}
+  insertComment(newComment, articles_id)
+    .then((comment) => {
+      res.status(201).send({ comment });
+    })
+    .catch(next);
+};
 
+const updateArticle = (req, res, next) => {
+  const { article_id } = req.params;
+  const voteDetail = req.body;
+  changeArticleVotes(article_id, voteDetail)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch(next);
+};
 
-
-module.exports = { getArticleByID, getArticle,getCommentByArtcId,postComment };
+module.exports = {
+  getArticleByID,
+  getArticle,
+  getCommentByArtcId,
+  postComment,
+  updateArticle,
+};
